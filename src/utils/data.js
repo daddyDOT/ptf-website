2b6f5ee1f03ab7138f6e59e9e5061a34899cd080
daddyDOT <ahmedspahiconline@gmail.com>
2024-06-02 17:23:03 +0200

    feat: Add data file with information about new topics

---
+++ src/utils/data.js
@@ -0,0 +1,17 @@
+const data = [
+    {
+        id: 1,
+        title: 'Sjajan rezultat naših studenata na takmičenju',
+        description: 'Studenti Politehničkog fakulteta Univerziteta u Zenici ostvarili su izuzetan uspjeh na regionalnom Univerzitetskom takmičenju održanom u Budvi u periodu od 15. do 19. maja tekuće godine. Na spomenutom takmičenju je učestvovalo preko 1700 studenata iz 6 država, te 20 gradova.\
+        U konkurenciji brojnih Univerziteta iz regiona, Ahmet Patković, Emina Šaćirbegović i Mateo Sušić su se posebno istakli u kategoriji znanja, gdje su dobili zadatak da predstave inovaciju za unapređenje bankarskih sistema. Naši studenti su napravili projekat pod nazivom “Dobbi”. “Dobbi” je predstavljen kao virtuelni asistent za mobilno bankarstvo koji bi svojim korisnicima pružio uvid na njihove finansije, pomogao im pri planiranju raspodjele njihovog dohotka, ograničavanju njihove dnevne potrošnje i stvaranjem mogućnosti za povrat novca od 3% prilikom kartičnog plaćanja. Ahmet, Emina i Mateo su za 90 minuta uspjeli da smisle ideju, naprave prezentaciju i esej, koji im je donio pobjedu na ovom takmičenju.\
+        Stručni žiri je jednoglasno proglasio ideju studenata Univerziteta u Zenici najboljom, što je donijelo našem univerzitetu prvo mjesto.\
+        Unija studenata Univerziteta u Zenici upućuje iskrenu zahvalnost menadžmentu Univerziteta u Zenici i sponzorima koji su omogućili našim studentima učešće na ovom prestižnom takmičenju. Vaša podrška je ključna za postizanje ovakvih rezultata i unapređenje studentskog standarda i aktivnosti.\
+        Ovaj uspjeh ne bi bio moguć bez podrške i sponzora: Tvornica cementa Kaknj, Neimax d.o.o Visoko, Boha namještaj Zenica, Širbegović inženjering, Medena Commerce d.o.o, HIFA-OIL d.o.o Tešanj, NUCAD d.o.o Bugojno. Madi d.o.o Tešanj i Almy d.o.o Zenica. Također,\
+        Povodom odlaska naših studenata na takmičenje u Budvu obratila se i predsjednica Unije studenata Larisa Gadžun:\
+        “Drago mi je da mogu još jednom potvrditi da Univerzitet u Zenici njeguje talente i pruža podršku vrijednim studentima. Naši studenti su osvojili prvo mjesto u kategoriji znanja na Univerzijadi, što je dokaz njihove predanosti i kvaliteta našeg obrazovanja. Moram pohvaliti Uniju studenata, koja se ponovo pokazala kao ključan faktor u ovakvim aktivnostima. Trudili smo se da našim studentima pružimo maksimalnu podršku kroz organizaciju i motivaciju.”\
+        Čestitamo svim učesnicima i zahvaljujemo se na sjajnoj prezentaciji našeg Univerziteta. Ova pobjeda je dokaz kvalitetnog rada i posvećenosti naših studenata i nastavnog osoblja Univerziteta u Zenici.\
+        '
+    },
+];
+
+export default data;
