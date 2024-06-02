98507bb297360e151bd2a4b251680dc1f03565da
daddyDOT <ahmedspahiconline@gmail.com>
2024-06-02 23:26:01 +0200

    refactor: Improve layout structure and styling in Layout component

--- src/components/Layout/children/BottomBar.jsx
+++ src/components/Layout/children/BottomBar.jsx
@@ -1,14 +1,14 @@
 const BottomBar = () => {
   return (
-    <div className="w-full bg-slate-200">
-        <div className="max-w-6xl px-5 mx-auto flex justify-between">
-            <div className="flex gap-5 px-3">
-                <p className="text-[#131A2D] py-3 font-medium">Početna</p>
-                <p className="text-[#131A2D] py-3 font-medium">O nama</p>
-                <p className="text-[#131A2D] py-3 font-medium">Kontakt</p>
+    <div className="w-full bg-slate-200 h-[5vh]">
+        <div className="max-w-6xl px-5 mx-auto flex justify-between h-full">
+            <div className="flex self-center gap-5 px-3">
+                <p className="text-[#131A2D] font-medium">Početna</p>
+                <p className="text-[#131A2D] font-medium">O nama</p>
+                <p className="text-[#131A2D] font-medium">Kontakt</p>
             </div>
-            <div>
-                <p className="text-[#131A2D] py-3 px-3 font-medium">© 2024. Sva prava zadržana.</p>
+            <div className="self-center">
+                <p className="text-[#131A2D] px-3 font-medium">© 2024. Sva prava zadržana.</p>
             </div>
         </div>
     </div>
