3746cfc20ed979307bbdac16d59c7508d8de9c3e
daddyDOT <ahmedspahiconline@gmail.com>
2024-06-03 05:05:12 +0200

    refactor: Update Slideshow component to handle null data values and improve layout and positioning

--- src/components/Layout/children/Slideshow.jsx
+++ src/components/Layout/children/Slideshow.jsx
@@ -1,37 +1,54 @@
 import { Button } from "@nextui-org/react"
-import data from "../../../utils/data"
-import { useEffect, useState } from "react"
+import data, { sliderSettings } from "../../../utils/data"
+import { useEffect, useRef, useState } from "react"
+import Slider from "react-slick";
 
 const Slideshow = () => {
     const [news, setNews] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const handlePrevSlide = () => {
+        slider.current.slickPrev();
+        currentPage > 1 ? setCurrentPage(currentPage - 1) : setCurrentPage(3);
+    };
+    const handleNextSlide = () => {
+        slider.current.slickNext();
+        currentPage < 3 ? setCurrentPage(currentPage + 1) : setCurrentPage(1);
+    };
+
     useEffect(() => {
         fetch('http://localhost:3000/news.json')
             .then(response => response.json())
             .then(data => setNews(data));
     }, []);
 
+    const slider = useRef(null);
+
     useEffect(() => {
-        console.log(news);
-    }, [news]);
+        console.log("currentPage", currentPage);
+    }, [currentPage]);
 
   return (
-    <div className="w-full" id="slideshow">
-        <div className="max-w-6xl mx-auto h-[81vh]">
+    <>
+        <div className="w-[100vw] absolute top-auto z-[2] h-[81vh]" id="slide-overlay">
+        <div className="flex items-center justify-center w-full h-full">
+
+        <div className="max-w-6xl mx-auto">
             <div className="flex justify-between items-center h-full gap-10 px-7">
                 <div className="w-[70%] lg:w-[50%] h-full flex flex-col justify-center items-start">
-                    <h1 className="text-5xl font-bold text-slate-100">{data[0]?.title}</h1>
-                    <p className="text-slate-100 py-5 line-clamp-3 max-h-[90px]">{data[0]?.description}</p>
+                    <h1 className="text-5xl font-bold text-slate-100 !z-[10]">{news[currentPage-1].title}</h1>
+                    <p className="text-slate-100 py-5 line-clamp-3 max-h-[90px] !z-[10]">{news[currentPage-1].description}</p>
                     
                     <div className="flex gap-10 flex-row-reverse absolute h-[70vh]">
                         <div className="w-full h-full flex flex-row-reverse justify-end items-end py-14 gap-[4rem]">
-                            <div className="text-white flex items-center" id="arrow-right">
+                            <a className="text-white flex items-center" id="arrow-right" onClick={() => handleNextSlide()}>
                                 <span className="mr-[3rem] text-white" id="arrow-right-line">&#8213;</span>
                                 <span className="text-white" id="arrow-right-anchor">&#10230;</span>
-                            </div>
-                            <div className="text-white flex items-center" id="arrow-left">
+                            </a>
+                            <a className="text-white flex items-center" id="arrow-left" onClick={() => handlePrevSlide()}>
                                 <span className="text-white" id="arrow-left-anchor">&#10229;</span>
                                 <span className="ml-[3rem] text-white" id="arrow-left-line">&#8213;</span>
-                            </div>
+                            </a>
                         </div>
                     </div>
                     <Button radius="full" className="bg-white text-[#131A2D] px-5 py-3 mt-5 font-medium">Saznaj više</Button>
@@ -41,7 +58,18 @@ const Slideshow = () => {
                 </div>
             </div>
         </div>
-    </div>
+
+        </div>
+        </div>
+        <Slider ref={slider} {...sliderSettings}>
+            {news.map((item) => (
+                <div className='w-full' id={`slideshow${item.id}`} key={item.id}>
+                    <div className="max-w-6xl mx-auto h-[81vh]">
+                    </div>
+                </div>
+            ))}
+        </Slider>
+    </>
   )
 }
 
