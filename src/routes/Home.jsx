1ed7f3175b089e4ca526908374aea8fbce8dfd0c
samedinho <samed.zelkanovic.23@size.ba>
2024-06-03 05:00:42 +0200

    Footer commit

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Slideshow from '../components/Layout/children/Slideshow'
 import News from '../components/News'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
     <div>
         <Slideshow />
         <News></News>
+        <Footer></Footer>
     </div>
   )
 }
